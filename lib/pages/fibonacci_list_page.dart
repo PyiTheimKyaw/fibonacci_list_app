e9e8a97d6c052e72881a1ddcbc369056abf48989
PyiTheimKyaw <92578990+PyiTheimKyaw@users.noreply.github.com>
2025-03-13 00:37:52 +0700

    set up folders and necessary files/packages

---
+++ lib/pages/fibonacci_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FibonacciListPage extends StatefulWidget {
+  const FibonacciListPage({super.key});
+
+  @override
+  State<FibonacciListPage> createState() => _FibonacciListPageState();
+}
+
+class _FibonacciListPageState extends State<FibonacciListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/utils/dimens.dart
@@ -0,0 +1,4 @@
+///App dimens
+const double kMargin16 = 16.0;
+const double kMargin18 = 18.0;
+const double kMargin20 = 20.0;

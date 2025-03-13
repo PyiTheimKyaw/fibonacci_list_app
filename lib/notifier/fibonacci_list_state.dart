87d083a8eb6485e4da9928930c81589e203a16db
PyiTheimKyaw <92578990+PyiTheimKyaw@users.noreply.github.com>
2025-03-13 16:14:46 +0700

    implemented fibonacci functional list

--- lib/notifier/fibonacci_list_state.dart
+++ lib/notifier/fibonacci_list_state.dart
@@ -1,9 +1,29 @@
+import '../models/fibonacci_model.dart';
+
 class FibonacciListState {
-  List<int> fibonacciList;
+  List<FibonacciModel> fibonacciList;
+  List<FibonacciModel> removedFibonacci;
+  FibonacciModel? latestAddedFibonacci;
+  FibonacciModel? latestRemovedFibonacci;
 
-  FibonacciListState({required this.fibonacciList});
+  FibonacciListState({
+    required this.fibonacciList,
+    required this.removedFibonacci,
+    this.latestAddedFibonacci,
+    this.latestRemovedFibonacci,
+  });
 
-  FibonacciListState copyWith({List<int>? fibonacciList}) {
-    return FibonacciListState(fibonacciList: fibonacciList ?? this.fibonacciList);
+  FibonacciListState copyWith({
+    List<FibonacciModel>? fibonacciList,
+    List<FibonacciModel>? removedFibonacci,
+    FibonacciModel? latestAddedFibonacci,
+    FibonacciModel? latestRemovedFibonacci,
+  }) {
+    return FibonacciListState(
+      fibonacciList: fibonacciList ?? this.fibonacciList,
+      removedFibonacci: removedFibonacci ?? this.removedFibonacci,
+      latestAddedFibonacci: latestAddedFibonacci ?? this.latestAddedFibonacci,
+      latestRemovedFibonacci: latestRemovedFibonacci ?? this.latestRemovedFibonacci,
+    );
   }
 }
